787c2f7099c7e2a4e6cc50d38b3bb0256f58db8c
Leo Ji <sj3026@nyu.edu>
2022-05-15 00:36:44 -0400

    indicator drawing

--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public enum TileType {Empty, PowerUp, Enemy, Obstacle, Door}
+public enum TileType {Empty, PowerUp, Enemy, Obstacle, Door, Player}
 /* represent each tile in the tile map*/
 public class Unit {
     private TileType type;
@@ -25,13 +25,18 @@ public class Unit {
 public class PathGenerator : MonoBehaviour
 {
 
-    [SerializeField] private Tilemap backgroundTileMap;
-    [SerializeField] private Tile emptyTile, enemyTile;
+    [SerializeField] private Tilemap backgroundTileMap, indicatorTileMap;
+    [SerializeField] private Tile emptyTile, enemyTile, playerTile, indicatorTile;
     private Unit[,] map = new Unit[3,11];
+
+    private PlayerControl player;
+
+    //getters & setters
+    public Unit[,] Map {get=>map;}
     
-    void Start()
+    void Awake() 
     {
-        //start from the middle bottom of the screen
+        //build the map
         Vector3Int startPos = backgroundTileMap.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2 - 1,0,0)));
 
         for (int x = 0; x < map.GetLength(0); x++) {
@@ -39,7 +44,13 @@ public class PathGenerator : MonoBehaviour
                 map[x,y] = new Unit(startPos + new Vector3Int(x,y,0));
             }
         }
+    }
 
+    void Start()
+    {   
+        player = FindObjectOfType<PlayerControl>();
+        //start from the middle bottom of the screen
+        map[player.CurrentIndexX, player.CurrentIndexY].Type = TileType.Player;
         drawMap();
     }
 
@@ -63,23 +74,55 @@ public class PathGenerator : MonoBehaviour
                 case TileType.Enemy:
                     backgroundTileMap.SetTile(unit.Pos, enemyTile);
                     break;
+                case TileType.Player:
+                    backgroundTileMap.SetTile(unit.Pos, playerTile);
+                    break;
             }
         }
+    }
+
+    /* to draw the indicator on map with specified Vector3Int position that shows as indexes
+       since it shows where player will move next, it will always be 1 index higher in y from position*/
+    public void drawIndicator() {
+        indicatorTileMap.SetTile(Map[player.MovingDestination.x, player.MovingDestination.y+1].Pos, indicatorTile);
+    }
+
+    /* delete indicator that drawn last time, so that there's only be one indicator */
+    public void deleteIndicator(Vector3Int indicatorPos) {
+        indicatorTileMap.SetTile(Map[indicatorPos.x, indicatorPos.y+1].Pos, null);
     }
 
     /* replace and generate a new row at the top of the map*/
     public void generateRow(TileType column1Type, TileType column2Type, TileType column3Type) {
         shiftDownward();
-        map[0,map.GetLength(1)-1].Type = column1Type;
-        map[1,map.GetLength(1)-1].Type = column2Type;
-        map[2,map.GetLength(1)-1].Type = column3Type;
+        Map[0,map.GetLength(1)-1].Type = column1Type;
+        Map[1,map.GetLength(1)-1].Type = column2Type;
+        Map[2,map.GetLength(1)-1].Type = column3Type;
     }
 
     /* shift the whole map down for 1 cell*/
     public void shiftDownward()
     {
-        for (int x = 0; x < map.GetLength(0); x++)
-            for (int y = 0; y < map.GetLength(1)-1; y++)
-                map[x,y].duplicate(map[x,y+1]);
+        for (int x = 0; x < Map.GetLength(0); x++)
+            for (int y = 0; y < Map.GetLength(1)-1; y++)
+                Map[x,y].duplicate(Map[x,y+1]);
+    }
+
+    /* check if pos is inside the map */
+    public bool checkValidIndex(int indexX, int indexY) {
+        if (indexX < 0 || indexX >= map.GetLength(0))
+            return false;
+        if (indexY < 0 || indexY >= map.GetLength(1))
+            return false;
+        return true;
+    }
+
+    /* check if destination is walkable */
+    public bool checkWalkable(int indexX, int indexY) {
+        if (checkValidIndex(indexX, indexY))
+            if (player.CurrentIndexX+1 >= indexX && player.CurrentIndexX-1 <= indexX)
+                if (player.CurrentIndexY+1 >= indexY && player.CurrentIndexY-1 <= indexY)
+                    return true;
+        return false;
     }
 }
---
+++ Assets/Scripts/Player/PlayerStateUpgrade.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateUpgrade : PlayerStateBase
+{
+    public override void EnterState(PlayerControl player) {}
+    public override void UpdateState(PlayerControl player) {}
+    public override void LeaveState(PlayerControl player) {}
+}
